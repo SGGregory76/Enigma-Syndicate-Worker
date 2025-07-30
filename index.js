c9024aa37ad4411d467e318ffd37d755e6c952a0
SGGregory76 <sggregory76@gmail.com>
2025-07-30 03:49:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-import { getCardData } from './data.js';
+import { promptList } from './data.js';
 
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname === '/data') {
-      const json = await getCardData(env);
-      return new Response(JSON.stringify(json, null, 2), {
-        headers: { "Content-Type": "application/json" }
-      });
-    }
-    return new Response("Welcome to Enigma Syndicate!", {
+  async fetch(request) {
+    const prompt = promptList[0]; // Use your first prompt or dynamic one
+
+    return new Response(`Using prompt: ${prompt}`, {
       headers: { "Content-Type": "text/plain" }
     });
   }
-}
+};
